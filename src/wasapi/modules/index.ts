5da59935a231a14e2a1156e042aed2878a648ada
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-26 11:25:24 -0500

    barrel file modules

---
+++ src/wasapi/modules/index.ts
@@ -0,0 +1,7 @@
+import { CampaignsModule } from "./campaigns";
+import { ContactsModule } from "./contacts";
+import { BotModule } from "./bot";
+import { CustomFieldsModule } from "./customFields";
+import { FunnelsModule } from "./funnels";
+
+export { CampaignsModule, ContactsModule, BotModule, CustomFieldsModule, FunnelsModule };
