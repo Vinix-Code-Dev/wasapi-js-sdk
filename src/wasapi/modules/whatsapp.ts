d64c0b7ad19fa33a62a748d151b9a5fc332c48bf
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-28 18:06:37 -0500

    feat: try catch sendMessage

--- src/wasapi/modules/whatsapp.ts
+++ src/wasapi/modules/whatsapp.ts
@@ -2,13 +2,19 @@
 
 import { AxiosClient } from "../client";
 
+
 export class WhatsappModule {
     constructor(private client: AxiosClient) { }
 
-    //posthttps://api-ws.wasapi.io/api/v1/whatsapp-messages  params message, wa_id, from_id 
-    async sendMessage(message: string, wa_id: string, from_id: string): Promise<any> {
-        const response = await this.client.post('/whatsapp-messages', { message, wa_id, from_id });
-        console.log(response.data);
-        return response.data;
+    //posthttps://api-ws.wasapi.io/api/v1/whatsapp-messages  params message, wa_id, from_id  crea un try catch para manejar el error
+    async sendMessage(params: { message: string, wa_id: string, from_id: string }): Promise<any> {
+        try {
+            const response = await this.client.post('/whatsapp-messages', params);
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error al enviar el mensaje:', error);
+            throw error;
+        }
     }
 }
