1661325eb8b1fd50f0898839cdfd36eb14cd7fe9
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-28 18:06:01 -0500

    feat: add constants for sending test messages

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+//constantes de prueba para el envio de mensajes
+
+const constants = {
+    MY_FROM_ID: '11689',
+    CLIENT_WA_ID: '573165170740',
+    MESSAGE: 'Mensaje de prueba'
+}
+
+export default constants;
