9c7415c017b6d16c0853859f569c9df2c3d0ff0d
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-28 19:57:46 -0500

    feat: agregar constantes para UUID de plantillas y archivo PDF

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,9 @@
 const constants = {
     MY_FROM_ID: '11689',
     CLIENT_WA_ID: '573165170740',
-    MESSAGE: 'Mensaje de prueba'
+    MESSAGE: 'Mensaje de prueba',
+    TEMPLATE_UUID: '4d70b752-d6ed-4aa7-96e8-a693c07bff7f',
+    TEMPLATE_UUID_FILE_PDF: '207b0c09-5cae-439c-b93c-4b1bdbeeca22'
 }
 
 export default constants;
