f036a4955ed2b7daac623eb410bf28c8fe96119a
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-28 19:59:33 -0500

    feat: eliminar configuración de dotenv y cliente Wasapi, y ejecutar ejemplos de creación de contactos

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,3 @@
-import dotenv from 'dotenv';
-import { WasapiClient } from './wasapi';
-import constants from './constants';
+import {runAllExamples} from '../examples/contacts/createContact';
 
-
-dotenv.config();
-
-const clientWasapi = new WasapiClient(process.env.API_KEY_WASAPI || '');
-
-
-// const message = await clientWasapi.whatsapp.sendMessage({
-//     message: constants.MESSAGE,
-//     wa_id: constants.CLIENT_WA_ID,
-//     from_id: constants.MY_FROM_ID
-// });
-
+runAllExamples();
