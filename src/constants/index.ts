588a535a57ea81ab1781e264019debbfda1da281
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-29 11:10:15 -0500

    feat: actualizar constante TEMPLATE_UUID_FILE_PDF a TEMPLATE_UUID_EXAMPLE_VARIABLES en el índice de constantes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,7 @@ const constants = {
     CLIENT_WA_ID: '573165170740',
     MESSAGE: 'Mensaje de prueba',
     TEMPLATE_UUID: '4d70b752-d6ed-4aa7-96e8-a693c07bff7f',
-    TEMPLATE_UUID_FILE_PDF: '207b0c09-5cae-439c-b93c-4b1bdbeeca22'
+    TEMPLATE_UUID_EXAMPLE_VARIABLES: 'b23257ae-fd97-4a31-9ee0-27a0032bd88d'
 }
 
 export default constants;
