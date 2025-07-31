fec8d55fd11c2dc02e499453e88cfe66afe3ee02
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-07-31 15:08:00 -0500

    refactor: actualizar el módulo Campaigns para manejar respuestas de campañas y errores

--- src/wasapi/modules/campaigns.ts
+++ src/wasapi/modules/campaigns.ts
@@ -1,29 +1,38 @@
 import { IModule } from "../interfaces/IModule";
 import { AxiosClient } from "../client";
-import { Campaign } from "../models";
+import { ErrorResponseAllCampaigns, ResponseAllCampaigns } from "../models/response/AllCampaigns.model";
+import { ErrorResponseCampaignById, ResponseCampaignById } from "../models/response/CampaignById.model";
 
 
-export class CampaignsModule implements IModule<Campaign> {
+export class CampaignsModule implements IModule {
     constructor(private client: AxiosClient) { }
 
-    async getAll(): Promise<Campaign[]> {
-        const response = await this.client.get('/campaigns');
-        console.log(response.data);
-        return response.data;
+    async getAll(): Promise<ResponseAllCampaigns> {
+        try {
+            const response = await this.client.get('/campaigns');
+            return response.data as ResponseAllCampaigns;
+        } catch (error) {
+            console.error('Error al obtener todas las campañas:', error);
+            throw error as ErrorResponseAllCampaigns;
+        }
     }
 
-    async getById(id: string): Promise<Campaign> {
-        const response = await this.client.get(`/campaigns/${id}`);
-        console.log(response.data);
-        return response.data;
+    async getById(id: string): Promise<ResponseCampaignById> {
+        try {
+            const response = await this.client.get(`/campaigns/${id}`);
+            return response.data as ResponseCampaignById;
+        } catch (error) {
+            console.error('Error al obtener la campaña:', error);
+            throw error as ErrorResponseCampaignById;
+        }
     }
 
-    async create(data: any): Promise<Campaign> {
-        console.log('Actualmente no esta disponible la creacion de campañas');
+    async create(data: any): Promise<void> {
+        //console.log('Actualmente no esta disponible la creacion de campañas');
         throw new Error('Creación de campañas no implementada');
     }
 
-    async update(data: { id: string, data: any }): Promise<Campaign> {
+    async update(data: { id: string, data: any }): Promise<void> {
         console.log('Actualmente no esta disponible la actualizacion de campañas');
         throw new Error('Actualización de campañas no implementada');
     }
