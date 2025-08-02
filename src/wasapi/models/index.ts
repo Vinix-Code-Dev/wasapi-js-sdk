ab3bfb64707b660537fdc044a48141d800e0bc37
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-08-02 11:55:24 -0500

    feat: agregar exportaciones de los modelos template y message en index.ts

--- src/wasapi/models/index.ts
+++ src/wasapi/models/index.ts
@@ -1,5 +1,7 @@
 export * from './request/contact.model';
 export * from './request/label.model';
 export * from './request/customField.model';
+export * from './request/template.model';
+export * from './request/message.model';
 
 
