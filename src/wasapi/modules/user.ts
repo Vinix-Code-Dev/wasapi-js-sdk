9dd6de1a54e7abf25415e0c351a32917a3c29f6e
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-08-02 09:43:50 -0500

    feat: agregar modelo UserResponse y actualizar el módulo User para manejar la respuesta del usuario

--- src/wasapi/modules/user.ts
+++ src/wasapi/modules/user.ts
@@ -1,15 +1,17 @@
-//create module for user
-
 import { AxiosClient } from "../client";
-import { User } from "../models";
+import { UserResponse } from "../models/response/user.model";
 
 export class UserModule {
     constructor(private client: AxiosClient) { }
 
-    //usuario de la cuenta  
-    async getUser(): Promise<User> {
-        const response = await this.client.get('/user');
-        console.log(response.data);
-        return response.data;
+    //GET https://api-ws.wasapi.io/api/v1/user obtener el usuario MANAGER de la cuenta
+    async getUser(): Promise<UserResponse> {
+        try {
+            const response = await this.client.get('/user');
+            return response.data as UserResponse;
+        } catch (error) {
+            console.error('Error al obtener el usuario:', error);
+            throw error;
+        }
     }
 }
