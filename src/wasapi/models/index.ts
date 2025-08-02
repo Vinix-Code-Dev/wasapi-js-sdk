308989b5e2560a8a222099e290184991ae15062b
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-08-02 10:04:17 -0500

    refactor: eliminar exportaciones de modelos bot, stage, user, message y flows en index.ts

--- src/wasapi/models/index.ts
+++ src/wasapi/models/index.ts
@@ -2,10 +2,4 @@ export * from './request/contact.model';
 export * from './request/label.model';
 export * from './request/customField.model';
 
-export * from './bot.model';
-export * from './stage.model';
-export * from './user.model';
-export * from './message.model';
-export * from './flows.model';
-
 
