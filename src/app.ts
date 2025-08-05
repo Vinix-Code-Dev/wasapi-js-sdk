6ecdadd53219dd87e80931a3d103232745b5a958
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-08-05 15:04:31 -0500

    feat: configurar servidor Express y registrar webhook de Wasapi

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
-import {runAllExamples} from '../examples/contacts/getAllContacts';
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { registerWasapiWebhook } from "examples/events/wasapiWebhook";
 
-runAllExamples();
+
+dotenv.config();
+const PORT = process.env.PORT || '3000'
+const app = express();
+app.use(bodyParser.json());
+
+// Registrar webhook de Wasapi
+registerWasapiWebhook(app, process.env.API_KEY_WASAPI || "");
+
+// Arrancar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`); 
+});
+
