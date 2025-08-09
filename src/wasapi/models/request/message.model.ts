fa5bf72a5bcdccff99dc46c2ba97cde216058e7e
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-08-09 12:12:57 -0500

    feat: agregar nueva interfaz SendAttachmentParams para mejorar la estructura de envío de archivos adjuntos en Wasapi

--- src/wasapi/models/request/message.model.ts
+++ src/wasapi/models/request/message.model.ts
@@ -1,10 +1,10 @@
-export interface SendMessage{
+export interface SendMessage {
     message: string;
     wa_id: string;
     from_id?: string;
 }
 
-export interface SendAttachment{
+export interface SendAttachment {
     from_id: number;
     wa_id: string;
     file: 'image_url' | 'video_url' | 'document_url' | 'audio_url';
@@ -12,6 +12,14 @@ export interface SendAttachment{
     video_url?: string;
     document_url?: string;
     audio_url?: string;
+    caption?: string;
+    filename?: string;
+}
+
+export interface SendAttachmentParams {
+    from_id: string;
+    wa_id: string;
+    filePath: string;
     caption?: string;
     filename?: string;
 }
