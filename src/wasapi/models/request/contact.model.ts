cdcba6ea864ec1d54262c50e56356a0cda3666b5
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-08-28 17:16:46 -0500

    Enhance contact model by adding UpdateContact interface and importing necessary types for custom fields and labels.

--- src/wasapi/models/request/contact.model.ts
+++ src/wasapi/models/request/contact.model.ts
@@ -1,4 +1,4 @@
-import { BaseContact } from "../shared/contact.model";
+import { BaseContact, ContactCustomField, ContactLabel } from "../shared/contact.model";
 
 export interface CreateContact extends BaseContact {
     blocked?: boolean;
@@ -6,5 +6,19 @@ export interface CreateContact extends BaseContact {
     custom_fields?: Record<string, unknown>;
 }
 
+export interface UpdateContact {
+    first_name?: string;
+    last_name?: string;
+    email?: string;
+    country_code?: string;
+    phone?: string;
+    notes?: string;
+    blocked?: boolean | number; 
+    unsubscribed?: boolean | number;
+    labels?: ContactLabel[];
+    custom_fields?: ContactCustomField[] | Record<string, any>;
+
+}
+
 export interface SearchContactParams { search?: string; labels?: number; page?: number; }
-export interface UpdateContactParams { wa_id: string; data: CreateContact }
+export interface UpdateContactParams { wa_id: string; data: UpdateContact }
