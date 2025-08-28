58408615b71b7545229cf9334a4f5fbe19302622
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-08-28 17:26:07 -0500

    Refactor UpdateContact interface to use CreateContact type in UpdateContactParams, removing the previous UpdateContact definition for clarity.

--- src/wasapi/models/request/contact.model.ts
+++ src/wasapi/models/request/contact.model.ts
@@ -6,19 +6,6 @@ export interface CreateContact extends BaseContact {
     custom_fields?: Record<string, unknown>;
 }
 
-export interface UpdateContact {
-    first_name?: string;
-    last_name?: string;
-    email?: string;
-    country_code?: string;
-    phone?: string;
-    notes?: string;
-    blocked?: boolean | number; 
-    unsubscribed?: boolean | number;
-    labels?: ContactLabel[];
-    custom_fields?: ContactCustomField[] | Record<string, any>;
-
-}
 
 export interface SearchContactParams { search?: string; labels?: number; page?: number; }
-export interface UpdateContactParams { wa_id: string; data: UpdateContact }
+export interface UpdateContactParams { wa_id: string; data: CreateContact }
